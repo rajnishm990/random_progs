eb6685134104856e88f660b4f60970d3b333787b
rajnish <rajnishm990@gmail.com>
2025-07-21 11:51:37 +0530

    sys_track

---
+++ sys_tracker/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI , WebSocket 
+from .utils import get_metrics
+import asyncio
+
+app = FastAPI()
+
+app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    data = get_metrics()
+    await websocket.send_json(data)
+    await asyncio.sleep(1)
+
